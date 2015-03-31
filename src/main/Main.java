ed75bc2aa3c6d80aba2f24f270277cbc5ef1cf72
Liam De Grey <computernerddegrey@gmail.com>
2015-04-01 12:28:40 +1300

    modified main to be able to take in many files at a time

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,11 +7,45 @@ import logic.TreeBuilder;
  * Created by Liam on 29-Mar-15.
  */
 public class Main {
+	private static final int TRAININGFILE = 0;
+	private static final int TESTFILE = 1;
+	private static final int RUNS = 2;
+	private static final String APPENDRUN = "-run";
+
+	private static void test(String trainingFile, String testFile) {
+		TreeBuilder tree = new TreeBuilder(trainingFile);
+        new TestInstances(tree.getRoot(), testFile);
+	}
+
+	private static String[] getFileNames(String trainingFile, String testFile, int run) {
+		String[] trainingName;
+    	String[] testName;
+        trainingName = trainingFile.split("\\.");
+        testName = testFile.split("\\.");
+
+        return new String[] {
+        		trainingName[0]+APPENDRUN+String.format("%02d", run)+"."+trainingName[1],
+        		testName[0]+APPENDRUN+String.format("%02d", run)+"."+testName[1]
+        				};
+	}
 
     public static void main(String[] args) {
-        if (args.length > 1) {
-            TreeBuilder tree = new TreeBuilder(args[0]);
-            new TestInstances(tree.getRoot(), args[1]);
-        }
+    	if (args.length > 1) {
+    		String trainingFile = args[TRAININGFILE];
+    		String testFile = args[TESTFILE];
+
+    		if (args.length < 3) {
+    			test(trainingFile, testFile);
+    		} else {
+    			int runs = Integer.parseInt(args[RUNS]);
+
+    			String[] fileNames;
+    	        for (int run = 1; run < runs + 1; run++){
+    	        	fileNames = getFileNames(trainingFile, testFile, run);
+    	        	System.out.println(fileNames[0]+", "+fileNames[1]);
+    	        	test(fileNames[TRAININGFILE], fileNames[TESTFILE]);
+    	        }
+    		}
+    	}
     }
 }
